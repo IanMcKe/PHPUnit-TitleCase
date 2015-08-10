99b7e0e8b7d90785f35df9f1d907e8f92a8a3f4a
Mike Chastain & Ian McKenney <student@epicodus.com>
2015-08-10 10:42:12 -0700

    create app.php

--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -4,7 +4,7 @@
     class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
     {
 
-        //Capitalize the first letter with a single word.
+//Capitalize the first letter with a single word.
         function test_makeTitleCase_oneword()
         {
             //Arrange
@@ -18,6 +18,20 @@
             $this->assertEquals("Beowulf", $result);
         }
 
+//If given a multiple word string it should capitlaize every word in that string.
+        function test_make_TitleCase_multipleWords()
+        {
 
+            //Arrange
+            $test_TitleCaseGenerator = new TitleCaseGenerator;
+            $input = "the little mermaid";
+
+            //Act
+            $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+            //Assert
+            $this->assertEquals("The Little Mermaid", $result);
+
+        }
     }
 ?>
